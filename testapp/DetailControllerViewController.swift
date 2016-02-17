2bc5f2a7d76a1db785f15f1d81ec1b2688bcc93f
Usman Khawar <kh.usman.khawar@gmail.com>
2016-02-17 15:00:43 +0500

    detail commit

--- testapp/DetailControllerViewController.swift
+++ testapp/DetailControllerViewController.swift
@@ -20,11 +20,12 @@ class DetailControllerViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -32,6 +33,9 @@ class DetailControllerViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated);
+    }
 
 }
