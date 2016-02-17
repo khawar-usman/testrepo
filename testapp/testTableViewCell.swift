5eee20832c6ea50d7180205f5640d06e0c53d66a
Usman Khawar <kh.usman.khawar@gmail.com>
2016-02-17 15:34:04 +0500

    new file commit to new branch

---
+++ testapp/testTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  testTableViewCell.swift
+//  testapp
+//
+//  Created by Tin on 2/17/16.
+//  Copyright © 2016 Tin. All rights reserved.
+//
+
+import UIKit
+
+class testTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
